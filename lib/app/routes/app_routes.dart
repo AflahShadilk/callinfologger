d0dbba55e7cba76bffe82d8eee47e63c86fcb64b
Aflah Shadil k <shadilpml@gmail.com>
2026-04-25 09:38:09 +0530

    route added

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,8 @@
+abstract class AppRoutes {
+  static const home        = '/home';
+  static const contact    = '/contacts';
+  static const addContact = '/add-contact';
+  static const contactDetails = '/contact-detail';
+  static const callLogs   = '/call-logs';
+  static const recordCall = '/record-call';
+}
